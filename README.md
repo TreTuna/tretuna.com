26e4744543ab40441a21b6f1074856936fabdd7b
Tré Ammatuna <tre@tretuna.com>
2019-03-07 22:19:18 -0800

    Update info configs

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # TreTuna.com
 
-Welcome to the home of the Funky Fish, TrÃ© Tuna.
+Welcome to the home of the Funky Fish, Tré Tuna.
 
 This site is the testing grounds for several technologies and under heavy construction.
--- config/website.js
+++ config/website.js
@@ -1,28 +1,28 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
-  siteTitle: 'My Blog', // Navigation and Site Title
-  siteTitleAlt: 'The blog of an egghead creator', // Alternative Site title for SEO
-  siteTitleShort: 'Starter Blog', // short_name for manifest
-  siteUrl: 'https://your-site.io', // Domain of your site. No trailing slash!
+  siteTitle: 'Tre Tuna', // Navigation and Site Title
+  siteTitleAlt: 'The blog of Tre Tuna', // Alternative Site title for SEO
+  siteTitleShort: 'Tre Tuna', // short_name for manifest
+  siteUrl: 'https://www.tretuna.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
-  siteDescription: 'This is where they post things!',
-  author: 'Author', // Author for schemaORGJSONLD
-  organization: 'egghead.io LLC',
+  siteDescription: 'The home of the funky fish.',
+  author: 'Tre Tuna', // Author for schemaORGJSONLD
+  organization: 'tretuna.com',
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@eggheadio', // Twitter Username
+  userTwitter: '@tretuna', // Twitter Username
   ogSiteName: 'egghead.io Gatsby Starter Blog', // Facebook Site Name
   ogLanguage: 'en_US',
-  googleAnalyticsID: '',
+  googleAnalyticsID: 'UA-135898929-1',
 
   // Manifest and Progress color
   themeColor: '#5348FF',
   backgroundColor: '#2b2e3c',
 
   // Social component
-  twitter: 'https://twitter.com/eggheadio/',
-  twitterHandle: '@eggheadio',
-  github: 'https://github.com/eggheadio/',
-  linkedin: '',
+  twitter: 'https://twitter.com/tretuna/',
+  twitterHandle: '@tretuna',
+  github: 'https://github.com/tretuna/',
+  linkedin: 'https://linkedin.com/in/tretuna',
 }
